11aaa10fbbd958af21ccbc02fed012f1e02ce65a
johnudoudo <mercausafetyng@gmail.com>
2022-12-08 20:47:17 -0800

    my first line of c language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts
+/**
+ * main -print a line of code using puts
  *
  * Return: 0
 */
@@ -8,4 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+i}
