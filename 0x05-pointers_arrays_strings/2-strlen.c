24f92522aa21bf386ff632742b09d6589a121ff2
johnudoudo <mercausafetyng@gmail.com>
2022-12-19 08:35:58 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen - return the length of a sring
+ * _strlen - return the length of a string
  *
  * @s: char to check
  * Description: this will return the length of a string
