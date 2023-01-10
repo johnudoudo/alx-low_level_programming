ec8ce335816b831f072c7babcccb6d98a46b30d1
johnudoudo <mercausafetyng@gmail.com>
2023-01-10 02:12:50 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+ble File  16 lines (13 sloc)  226 Bytes
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
